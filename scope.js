75f2e88d23cf784c90f8763110dc1303f76404f1
unknown <ccxsungodzzx@gmail.com>
2014-09-22 20:28:28 +0800

    add $watch and $digest

---
+++ scope.js
@@ -0,0 +1,33 @@
+function Scope() {
+  this.$$watchers = [];
+}
+
+Scope.prototype.$watch = function(watchFn, listenerFn) {
+  var watcher = {
+    watchFn: watchFn,
+    listenerFn: listenerFn
+  };
+  this.$$watchers.push(watcher);
+};
+
+Scope.prototype.$digest = function() {
+  var self = this;
+  _.forEach(this.$$watchers, function (watcher) {
+    watcher.listenerFn.apply(self);
+  })
+};
+
+function noop() {} /* do nothing*/
+
+var oneScope = new Scope();
+
+oneScope.$watch(noop, function () {
+  console.log('listener 1')
+});
+
+oneScope.$watch(noop, function () {
+  console.log('listener 2')
+});
+
+oneScope.$digest();
+oneScope.$digest();
