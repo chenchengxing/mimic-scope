cf1f0ee25dcbe44ea6b7a8cf0506197a4ba8cd57
chenchengxing <ccxsungodzzx@gmail.com>
2014-09-24 14:22:21 +0800

    when dirty, continue digesting

--- scope.js
+++ scope.js
@@ -11,21 +11,40 @@ Scope.prototype.$watch = function(watchFn, listenerFn) {
 };
 
 Scope.prototype.$digest = function() {
-  var self = this;
-  _.forEach(this.$$watchers, function (watcher) {
-    watcher.listenerFn.call(self);
-  });
+  var dirty = false;
+  do {
+    dirty = this.$digestOnce();
+  } while(dirty);
 };
-
+Scope.prototype.$digestOnce = function () {
+  var self = this;
+  var dirty = false;
+  _.forEach(this.$$watchers, function (watcher) {
+    var newValue = watcher.watchFn.apply(self);
+    var oldValue = watcher.last;
+    if (newValue !== oldValue) {
+      watcher.listenerFn.call(self, newValue, oldValue);
+      watcher.last = newValue;
+      dirty = true;
+    }
+  });
+  return dirty;
+}
 function noop () {}
 var oneScope = new Scope();
-oneScope.$watch(noop, function () {
-  console.log('listener 1');
+oneScope.$watch(function () {
+  return this.kaka
+}, function () {
+  console.log('kaka change');
+});
+oneScope.$watch(function () {
+  return this.name;
+}, function (n, o) {
+  console.log('listener 1', n, o);
+  this.kaka = 'kaka';
 });
 
-oneScope.$watch(noop, function () {
-  console.log('listener 2');
-});
 
-oneScope.$digest();
+// oneScope.$digest();
+oneScope.name = '222';
 oneScope.$digest();
